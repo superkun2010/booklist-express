0aeb28fcd9ad657c4f91b57f72dbed645691b4e7
Liz Howard <liz.howard@galvanize.com>
2016-06-02 14:44:21 -0700

    fixed lack of modules

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var Booklist = require("./models/books.js").Booklist;
+var Book = require("./models/books.js").Book;
 
 //Put all books in this global booklist - don't create new booklists!
 var books = new Booklist();
